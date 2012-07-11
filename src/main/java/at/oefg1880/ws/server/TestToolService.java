7b3f1fbbc517eacc5695a74205cc00d2c7f5eaca
Maximilian Schremser <maxi@schremser.com>
2012-07-11 11:55:29 +0200

    need to look at testtoolservice war file

--- src/main/java/at/oefg1880/ws/server/TestToolService.java
+++ src/main/java/at/oefg1880/ws/server/TestToolService.java
@@ -1,21 +1,22 @@
 package at.oefg1880.ws.server;
-
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
 
 /**
  * Created by IntelliJ IDEA.
  * User: AT003053
- * Date: 10/07/12
- * Time: 08:09
+ * Date: 11.07.12
+ * Time: 11:28
  * To change this template use File | Settings | File Templates.
  */
 @WebService()
 public class TestToolService {
   @WebMethod
-  public String getFragebogen(String fragebogen) {
-    String result = "Hello, world, from " + fragebogen;
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
     System.out.println(result);
     return result;
   }
+
 }
